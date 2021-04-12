dd7f38062abbbc31af4f41bb65bee521852b2352
asha joseph <ashajo183@gmail.com>
2021-04-12 09:49:53 +0200

    Initial commit

---
+++ src/main/java/se/lexicon/shipping_cost_calculator/ShippingCostCalculatorApplication.java
@@ -0,0 +1,13 @@
+package se.lexicon.shipping_cost_calculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShippingCostCalculatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShippingCostCalculatorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/se/lexicon/shipping_cost_calculator/ShippingCostCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package se.lexicon.shipping_cost_calculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShippingCostCalculatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
